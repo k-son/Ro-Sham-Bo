1190f9b37c03010f97644f6b42974b5a9ef5780e
Jacek Jarczok <kson.eu@gmail.com>
2019-05-23 11:14:39 +0200

    Initial upload scripts.js Develop index.html, components.scss, javascriptStyles.scss and variables.scss

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const btnStartIns = document.getElementById("button--start--ins");
+const btnStartOut = document.getElementById("button--start--out");
+const btnPaperIns = document.getElementById("button--paper--ins");
+const btnPaperOut = document.getElementById("button--paper--out");
+const btnRockIns = document.getElementById("button--rock--ins");
+const btnRockOut = document.getElementById("button--rock--out");
+const btnScissorsIns = document.getElementById("button--scissors--ins");
+const btnScissorsOut = document.getElementById("button--scissors--out");
+const btnQuestion = document.getElementById("button--question");
+const btnSound = document.getElementById("button--sound");
+
+const indicatorPaperCpu = document.getElementById("indicator--paper--cpu");
+const indicatorPaperYou = document.getElementById("indicator--paper--you");
+const indicatorRockYou = document.getElementById("indicator--rock--you");
+const indicatorRockCpu = document.getElementById("indicator--rock--cpu");
+const indicatorScissorsYou = document.getElementById("indicator--scissors--you");
+const indicatorScissorsCpu = document.getElementById("indicator--scissors--cpu");
+
+const signalWinYou = document.getElementById("whoWins--you");
+const signalWinCpu = document.getElementById("whoWins--cpu");
+const signalPointYou = document.getElementById("pointFor--you");
+const signalPointCpu = document.getElementById("pointFor--cpu");
+const signalScoreBoxYou = document.getElementById("scoreBox--you");
+const signalScoreBoxCpu = document.getElementById("scoreBox--cpu");
+
