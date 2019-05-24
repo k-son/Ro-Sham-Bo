04c7beca3ea338ff966fe0abec46ea0c938fa580
Jacek Jarczok <kson.eu@gmail.com>
2019-05-24 14:49:20 +0200

    Develop scripts.js

--- scripts.js
+++ scripts.js
@@ -26,17 +26,11 @@ const signalDrawCpu = document.getElementById("draw--cpu");
 const signalScoreBoxYou = document.getElementById("scoreBox--you");
 const signalScoreBoxCpu = document.getElementById("scoreBox--cpu");
 
-let scoreYou = "";
-let scoreCpu = "";
+let scoreYou = 0;
+let scoreCpu = 0;
 
 
 ///// Functions
-
-/// Disactivate start button
-function disactivateStart() {
-  btnStartOut.classList.remove("illuminate--bg");
-  btnStartIns.removeEventListener("click", buttonsReady);
-}
 
 /// Option for new game
 function startNewGame() {
@@ -44,43 +38,56 @@ function startNewGame() {
   setTimeout (buttonsReady, 1200);
 }
 
-/// Switch off all illuminations and zero score board
-function clean() {
+/// Computer draw
+function drawCpu() {
+  const choices = ["paper", "rock", "scissors"];
+  let draw = Math.floor(Math.random() * 3);
+  return choices[draw];
+}
+
+/// Disactivate start button
+function disactivateStart() {
+  btnStartOut.classList.remove("illuminate--bg");
+  //btnStartIns.removeEventListener("click", buttonsReady);
+}
+
+/// Cleaning functions
+function illuminationsOff() {
   let illuminated = document.getElementsByClassName("illuminate");
   while (illuminated.length)
       illuminated[0].classList.remove("illuminate");
   let illuminatedBg = document.getElementsByClassName("illuminate--bg");
   while (illuminatedBg.length)
       illuminatedBg[0].classList.remove("illuminate--bg");
+}
 
+function zeroScores() {
   setTimeout (function() {document.getElementById("score--you--0").scrollIntoView()}, 1);
   setTimeout (function() {document.getElementById("score--cpu--0").scrollIntoView()}, 600);
-
   scoreYou = 0;
   scoreCpu = 0;
 }
 
-/// Prepare and handle player's choice
-function buttonsReady() {
-  disactivateStart();
-  btnStartIns.addEventListener("click", startNewGame);
+/// Switch off all illuminations and zero score board
+function clean() {
+  illuminationsOff();
+  zeroScores();
+}
 
-  btnPaperOut.classList.add("illuminate--bg");
-  btnRockOut.classList.add("illuminate--bg");
-  btnScissorsOut.classList.add("illuminate--bg");
+/// Pulsat signal
+function pulsate(el) {
+  el.classList.add("illuminate");
+  setTimeout (function() {el.classList.remove("illuminate")}, 300);
+  setTimeout (function() {el.classList.add("illuminate")}, 600);
+  setTimeout (function() {el.classList.remove("illuminate")}, 900);
+  setTimeout (function() {el.classList.add("illuminate")}, 1200);
+  setTimeout (function() {el.classList.remove("illuminate")}, 1500);
+}
 
-  btnPaperIns.onclick = function() {
-    round("paper");
-    disableButtons();
-  }
-  btnRockIns.onclick = function() {
-    round("rock");
-    disableButtons();
-  }
-  btnScissorsIns.onclick = function() {
-    round("scissors");
-    disableButtons();
-  }
+/// Illuminate scoreBox
+function illuminateScoreBox(el) {
+  el.classList.add("illuminate--bg");
+  setTimeout (function() {el.classList.remove("illuminate--bg")}, 1500);
 }
 
 /// Disable and switch off buttons
@@ -94,11 +101,28 @@ function disableButtons() {
   btnScissorsOut.classList.remove("illuminate--bg");
 }
 
-/// Computer draw
-function drawCpu() {
-  const choices = ["paper", "rock", "scissors"];
-  let draw = Math.floor(Math.random() * 3);
-  return choices[draw];
+
+
+/// Buttons ready - prepare and handle player's choice
+function buttonsReady() {
+  btnStartIns.addEventListener("click", startNewGame);
+
+  btnPaperOut.classList.add("illuminate--bg");
+  btnRockOut.classList.add("illuminate--bg");
+  btnScissorsOut.classList.add("illuminate--bg");
+
+  btnPaperIns.onclick = function() {  
+    disableButtons();
+    round("paper");
+  }
+  btnRockIns.onclick = function() {
+    disableButtons();
+    round("rock");
+  }
+  btnScissorsIns.onclick = function() {
+    disableButtons();
+    round("scissors");
+  }
 }
 
 
@@ -122,7 +146,6 @@ function round(choice) {
     indicatorScissorsCpu.classList.add("illuminate--bg");
   }
 
-  
   switch (choiceYou) {
 
     case "paper":
@@ -155,53 +178,72 @@ function round(choice) {
       }
       break;
   }
-  
-
-}
-
-/// Pulsat signal
-function pulsate(el) {
-  el.classList.add("illuminate");
-  setTimeout (function() {el.classList.remove("illuminate")}, 400);
-  setTimeout (function() {el.classList.add("illuminate")}, 800);
-  setTimeout (function() {el.classList.remove("illuminate")}, 1200);
-  setTimeout (function() {el.classList.add("illuminate")}, 1600);
-  setTimeout (function() {el.classList.remove("illuminate")}, 2000);
 }
 
 
-/// Illuminate scoreBox
-function illuminateScoreBox(el) {
-  el.classList.add("illuminate--bg");
-  setTimeout (function() {el.classList.remove("illuminate--bg")}, 2000);
-}
-
-
-/// Point for a player
+/// Point for
 function pointForYou() {
-  ++scoreYou;
+  disableButtons();
   pulsate(signalPointYou);
   illuminateScoreBox(signalScoreBoxYou);
+  ++scoreYou;
+  //console.log(`your score is ${scoreYou}`);
   document.getElementById(`score--you--${scoreYou}`).scrollIntoView();
+  setTimeout (function() {illuminationsOff()}, 1500);
+  setTimeout (function() {buttonsReady()}, 1500);
 }
 
 function pointForCpu() {
-  ++scoreCpu;
+  disableButtons();
   pulsate(signalPointCpu);
   illuminateScoreBox(signalScoreBoxCpu);
+  ++scoreCpu;
+  //console.log(`cpu score is ${scoreCpu}`);
   document.getElementById(`score--cpu--${scoreCpu}`).scrollIntoView();
+  setTimeout (function() {illuminationsOff()}, 1500);
+  setTimeout (function() {buttonsReady()}, 1500);
 }
 
 function noPoints() {
+  disableButtons();
   pulsate(signalDrawYou);
   pulsate(signalDrawCpu);
+  setTimeout (function() {illuminationsOff()}, 1500);
+  setTimeout (function() {buttonsReady()}, 1500);
 }
 
 
-/// Scenario
-
-btnStartOut.classList.add("illuminate--bg");
-btnStartIns.addEventListener("click", buttonsReady);
 
 
+///// Game play
 
+start();
+
+function start() {
+  btnStartOut.classList.add("illuminate--bg");
+  btnStartIns.addEventListener("click", play);
+}
+
+function play() {
+  disactivateStart();
+
+  if (scoreYou == 5) {
+    console.log("no ten tego you");
+    
+    disableButtons();
+    pulsate(signalWinYou);
+    illuminateScoreBox(signalScoreBoxYou);
+    btnStartOut.classList.add("illuminate--bg");
+    
+  } else if (scoreCpu == 5) {
+    console.log("no ten tego cpu");
+  
+    disableButtons();
+    pulsate(signalWinCpu);
+    illuminateScoreBox(signalScoreBoxCpu);
+    btnStartOut.classList.add("illuminate--bg");
+    
+  } else {
+    buttonsReady();
+  }
+}
